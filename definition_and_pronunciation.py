1b2855c2d7bcec4cd3829f74ad9381be92cb1837
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-03 16:12:25 +0530

    Update definition_and_pronunciation.py

--- definition_and_pronunciation.py
+++ definition_and_pronunciation.py
@@ -15,17 +15,13 @@ pronunciations = 'pronunciations'
 definitions = 'definitions'
 strictMatch = 'false'
 
-pronunciation_url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.lower() + '?fields=' + pronunciations + '&strictMatch=' + strictMatch;
-pronunciation_response = requests.get(pronunciation_url, headers = {'app_id': app_id, 'app_key': app_key})
-json_pronunciation_response = json.loads(pronunciation_response.text)
+url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.lower()
+response = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+json_response = json.loads(response.text)
 
-definition_url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.lower() + '?fields=' + definitions + '&strictMatch=' + strictMatch;
-definition_response = requests.get(definition_url, headers = {'app_id': app_id, 'app_key': app_key})
-json_definition_response = json.loads(definition_response.text)
+pronunciation = str(json_response['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile'])
 
-pronunciation = str(json_pronunciation_response['results'][0]['lexicalEntries'][0]['entries'][0]['pronunciations'][0]['audioFile'])
-
-definition = str(json_definition_response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
+definition = str(json_response['results'][0]['lexicalEntries'][0]['entries'][0]['senses'][0]['definitions'][0])
 
 print("The definition for word %s is %s"%(word_id, definition))
 playsound.playsound(pronunciation, True)
