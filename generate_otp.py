044674b79f594d2aef256ce957ab445f5c0c5921
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-03 15:07:39 +0530

    Create generate_otp.py

---
+++ generate_otp.py
@@ -0,0 +1,18 @@
+#Program to generate OTP
+import requests
+import random
+
+def framework_authentication():
+	mobile_number = input("Enter mobile number: ")
+
+	random_number = random.randint(1000,9999)
+
+	response = requests.get('http://psms.goforsms.com/API/sms.php?username=srushtiimages&password=tecnics&from=WEBSMS&to=%s&msg=%s'%(mobile_number, random_number))
+
+	OTP = int(input("Enter OTP: "))
+
+	if random_number == OTP:
+		print("Access granted.")
+	else:
+		print("Access denied.")
+		
