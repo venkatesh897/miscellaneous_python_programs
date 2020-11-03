d2eff9e97f90fd9f72a8c1cb600407f3e39c6ab3
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-03 15:01:25 +0530

    Update change_image_background.py

--- change_image_background.py
+++ change_image_background.py
@@ -1,3 +1,4 @@
+#Program to change background color of image from white to black
 from PIL import Image
 
 img = Image.open("Screenshot-from-2018-10-16-09-31-31.png")
@@ -8,17 +9,13 @@ datas = img.getdata()
 new_image_data = []
 for item in datas:
     # change all white (also shades of whites) pixels to yellow
-    if item[0] in list(range(0, 192)):
-        new_image_data.append((255, 255, 255))
+    if item[0] in list(range(190, 255)):
+        new_image_data.append((0, 0, 0))
     else:
         new_image_data.append(item)
-
         
-# update image data
 img.putdata(new_image_data)
 
-# save new image
 img.save("test_image_altered_background.jpg")
 
-# show image in preview
 img.show()
