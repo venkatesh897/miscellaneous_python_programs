cd6b7a9b297c7b26d43b5d812be697f01fec173b
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-03 14:52:08 +0530

    Create temperature_of_given_city.py

---
+++ temperature_of_given_city.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+city = input('Enter city: ')
+response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=%s&appid=f9ba15284b25d77cf2aae3a2733bb72a&units=metric'%(city))
+
+weather_report = json.loads(response.text)
+
+print('Temperature in %s is '%(city),end="")
+print(weather_report['main']['temp'])
