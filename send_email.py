08f436a15e7f464197d0c72438d5dd51e8887792
venkatesh badarala <55230656+venkatesh897@users.noreply.github.com>
2020-11-03 14:58:45 +0530

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,19 @@
+ 
+import smtplib 
+   
+server = smtplib.SMTP('smtp.gmail.com', 587) 
+  
+server.starttls() 
+
+server.login("venkatesh.b.8.97@gmail.com", "aqwsderf3") 
+
+message = """From: <venkatesh.b.8.97@gmail.com>
+To: <venkateshbadarala99@gmail.com>
+MIME-Version: 1.0
+Content-type: text/html
+Subject: Test mail
+
+This mail is sent through python.
+"""
+server.sendmail("venkatesh.b.8.97@gmail.com", "venkateshbadarala99@gmail.com", message) 
+server.quit() 
